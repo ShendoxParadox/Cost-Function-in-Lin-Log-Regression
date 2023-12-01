bec7da16e5b271ba14047a8962b80ac68c04f409
ramez <rshendy@synapse-analytics.io>
2023-12-01 15:04:36 +0100

    modify visual to 2d, theta 1 and MSE

--- lin_log_regression.py
+++ lin_log_regression.py
@@ -1,7 +1,6 @@
 # %%
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 def linear_regression(X, y, learning_rate=0.01, epochs=1000):
     m, n = X.shape
@@ -51,36 +50,20 @@ plt.ylabel('Mean Squared Error (MSE)')
 plt.title('Convergence of Loss Function (MSE)')
 plt.show()
 
-# Visualize the regression line
-plt.scatter(X, y, label='Actual Data')
-plt.plot(X, y_pred, color='red', label='Regression Line')
-plt.xlabel('X')
-plt.ylabel('y')
-plt.title('Linear Regression with MSE')
-plt.legend()
-plt.show()
-
-# Visualize the cost function in 3D
+# Visualize the cost function for theta1
 theta1_vals = np.linspace(-10, 10, 100)
-theta2_vals = np.linspace(-1, 4, 100)
-mse_vals = np.zeros((len(theta1_vals), len(theta2_vals)))
+mse_vals = np.zeros(len(theta1_vals))
 
 for i, theta1 in enumerate(theta1_vals):
-    for j, theta2 in enumerate(theta2_vals):
-        theta[0] = theta1
-        theta[1] = theta2
-        y_pred = predict(X_b, theta, bias)
-        mse_vals[i, j] = np.mean((y_pred - y) ** 2)
+    theta[1] = theta1
+    y_pred = predict(X_b, theta, bias)
+    mse_vals[i] = np.mean((y_pred - y) ** 2)
 
-theta1_grid, theta2_grid = np.meshgrid(theta1_vals, theta2_vals)
-
-fig = plt.figure(figsize=(10, 6))
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(theta1_grid, theta2_grid, mse_vals.T, cmap='viridis', alpha=0.8)
-ax.set_xlabel('Theta1')
-ax.set_ylabel('Theta2')
-ax.set_zlabel('Mean Squared Error (MSE)')
-ax.set_title('Cost Function in 3D')
+# Visualize the cost function in 2D
+plt.plot(theta1_vals, mse_vals)
+plt.xlabel('Theta1')
+plt.ylabel('Mean Squared Error (MSE)')
+plt.title('Cost Function for Theta1')
 plt.show()
 
 # %%
